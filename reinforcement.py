e7d0630179267936abf0874ab0031d86fc73cce0
lilyQi-19 <lily@standrews-gv.com>
2024-04-18 20:02:08 +0700

    'human' mode

--- reinforcement.py
+++ reinforcement.py
@@ -3,10 +3,10 @@ import numpy as np
 import random
 from IPython.display import clear_output
 
-env = gym.make("Taxi-v3", render_mode="human")
+env = gym.make("Taxi-v3", render_mode="ansi")
 env.reset() # reset environment to a new, random state
 
-state = 328 # (taxi row, taxi column, passenger index, destination index)
+state = env.unwrapped.encode(3, 1, 2, 0) # (taxi row, taxi column, passenger index, destination index)
 print("State:", state)
 env.action_space
 
@@ -38,9 +38,8 @@ for i in range(1, 100001):
     done = False
     
     while not done:
-        print(epochs)
         if random.uniform(0, 1) < epsilon:
-            action = env.action_space # Explore action space
+            action = env.action_space.sample() # Explore action space
         else:
             action = np.argmax(q_table[state]) # Exploit learned values
 
@@ -63,6 +62,36 @@ for i in range(1, 100001):
         print(f"Episode: {i}")
 
 print("Training finished.\n")
+env.close()
+
+env = gym.make("Taxi-v3", render_mode="ansi")
+
+total_epochs, total_penalties = 0, 0
+episodes = 100
+
+for _ in range(episodes):
+    state,info = env.reset()
+    epochs, penalties, reward = 0, 0, 0
+    
+    done = False
+    
+    while not done:
+        action = np.argmax(q_table[state])
+        state, reward, done, truncated, info = env.step(action) 
+        env.action_space
+
+        if reward == -10:
+            penalties += 1
+
+        epochs += 1
+
+    total_penalties += penalties
+    total_epochs += epochs
+
+print(f"Results after {episodes} episodes:")
+print(f"Average timesteps per episode: {total_epochs / episodes}")
+print(f"Average penalties per episode: {total_penalties / episodes}")
+
 
 # for _ in range(1000):
 #     action = env.action_space.sample()  # agent policy that uses the observation and info
